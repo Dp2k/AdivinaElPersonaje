1302b0958834f9c2daf7b302ca4271584b77f5b7
Dp2k <danielo_pacheco@hotmial.com>
2021-11-26 22:11:42 -0600

    Agregamos El Manejador De WebSockets (Socket.io)

---
+++ app/src/main/java/com/example/adivinaelpersonaje/SocketHandler.kt
@@ -0,0 +1,25 @@
+package com.example.adivinaelpersonaje
+
+import io.socket.client.IO
+import io.socket.client.Socket
+import java.net.URISyntaxException
+
+object SocketHandler {
+    lateinit var  mSocket: Socket
+
+    @Synchronized
+    fun setSocket(){
+        try{
+            mSocket = IO.socket("http://guesswho.danielpacheco.com.mx:3000")
+        }catch(e: URISyntaxException){
+
+        }
+    }
+
+    @Synchronized
+    fun getSocket():Socket{
+        return  mSocket
+    }
+
+
+}
