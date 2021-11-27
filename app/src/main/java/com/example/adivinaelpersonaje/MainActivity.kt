af407fcfece73dc3ea917ef3d2a847679b113993
Axel Israel <al210900@edu.uaa.mx>
2021-11-27 11:35:30 -0600

    Corregimos el MainActivity y ya inicia la aplicacion Ya redirecciona a la actividad LoginActivity.kt

--- app/src/main/java/com/example/adivinaelpersonaje/MainActivity.kt
+++ app/src/main/java/com/example/adivinaelpersonaje/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.example.adivinaelpersonaje
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.login)
+        setContentView(R.layout.fragment_inicio)
+        var btnjugar = findViewById<Button>(R.id.Game1vs1)
 
+        btnjugar.setOnClickListener{
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
